5d24c0e9a513bf419f81ea0cdade8f60380c96c3
yangshuai56 <yangshuai5610@gmail.com>
2024-11-28 17:02:09 +0800

    area code test

---
+++ p9-area-code-trag/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	areaCode1 := "966"
+	areaCode2 := "852"
+
+	areaCode4 := 966
+	areaCode5 := 852
+
+	phoneNumber1 := "966501234567"
+	phoneNumber2 := "+966501234567"
+	phoneNumber3 := "+852801234567"
+
+	url1 := "https://www.baidu.com/966"
+	url2 := "https://www.baidu.com/852"
+
+	str1 := "fdff1966"
+	str2 := "fdff1852"
+
+	year1 := "1966"
+	year2 := "1852"
+
+	fmt.Println(areaCode1, areaCode2, areaCode4, areaCode5)
+	fmt.Println(phoneNumber1, phoneNumber2, phoneNumber3)
+	fmt.Println(url1, url2)
+	fmt.Println(str1, str2)
+	fmt.Println(year1, year2)
+
+}
